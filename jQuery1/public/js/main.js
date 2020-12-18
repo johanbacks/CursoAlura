38e487ada5ff93d2d837c0b536cb7451de2f152f
Johan Back <johan.backs@gmail.com>
2020-12-18 16:56:23 -0300

    chore: add contador

--- jQuery1/public/js/main.js
+++ jQuery1/public/js/main.js
@@ -1,3 +1,15 @@
 var frase = $(".frase").text();
 var numeroPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase").text(numeroPalavras)
+
+var campo = $(".campo-digitacao");
+campo.on("input", () => {
+ var conteudo = campo.val();
+
+ var qtdPalavras = conteudo.split(/\s+/).length - 1;
+ $("#contador-palavras").text(qtdPalavras)
+ 
+ var qtdCaracters =  conteudo.length ;
+ $("#contador-caracteres").text(qtdCaracters)
+
+});
