57e468155932a2bfa45a8d480eda95f80ad8113e
Johan Back <johan.backs@gmail.com>
2020-12-30 15:53:50 -0300

    chore: add cronometro/reiniciaJogo

--- jQuery1/public/js/main.js
+++ jQuery1/public/js/main.js
@@ -1,15 +1,56 @@
-var frase = $(".frase").text();
-var numeroPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase").text(numeroPalavras)
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
-campo.on("input", () => {
- var conteudo = campo.val();
 
- var qtdPalavras = conteudo.split(/\s+/).length - 1;
- $("#contador-palavras").text(qtdPalavras)
- 
- var qtdCaracters =  conteudo.length ;
- $("#contador-caracteres").text(qtdCaracters)
-
+$(function() {
+  atualizaTamanhoFrase();
+  inicializaContadores();
+  inicializaCronometro();
+  reniciaJogo();
 });
+
+function atualizaTamanhoFrase() {
+  var frase = $(".frase").text();
+  var numeroPalavras = frase.split(" ").length;
+  var tamanhoFrase = $("#tamanho-frase")
+    tamanhoFrase.text(numeroPalavras);
+}
+
+function inicializaContadores() {
+  campo.on("input", () => {
+    var conteudo = campo.val();
+   
+    var qtdPalavras = conteudo.split(/\s+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras)
+    
+    var qtdCaracters =  conteudo.length ;
+    $("#contador-caracteres").text(qtdCaracters)
+   });
+}
+
+function inicializaCronometro() {
+  var tempoRestante = $("#tempo-digitacao").text();
+  campo.one("focus" , function() {
+  var tempoID =  setInterval(function() {
+    tempoRestante--;
+    $("#tempo-digitacao").text(tempoRestante)
+    if(tempoRestante < 1) {
+      campo.attr("disabled", true)
+      clearInterval(tempoID)
+    }
+  },1000)
+})
+}
+
+
+function reniciaJogo() {
+  $("#reiniciar").click(function(){
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro()
+  })
+}
+$("#reiniciar").click(reniciaJogo);
+
