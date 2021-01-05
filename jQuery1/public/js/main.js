d86050e5306b84c2616a88fe6e434a4f814cebee
Johan Back <johan.backs@gmail.com>
2021-01-05 18:47:25 -0300

    chore: finish project

--- jQuery1/public/js/main.js
+++ jQuery1/public/js/main.js
@@ -35,13 +35,19 @@ function inicializaCronometro() {
     tempoRestante--;
     $("#tempo-digitacao").text(tempoRestante)
     if(tempoRestante < 1) {
-      campo.attr("disabled", true)
-      campo.toggleClass("campo-desativado")
       clearInterval(tempoID)
+      finalizaJogo();
     }
   },1000)
 })
 }
+function finalizaJogo(){
+    campo.attr("disabled", true)
+    campo.toggleClass("campo-desativado")
+    inserePlacar();
+}
+
+
 function inicializaMarcadores() {
   var frase = $(".frase").text();
   campo.on("input" , function() {
@@ -58,7 +64,39 @@ function inicializaMarcadores() {
 })
 
 }
+function inserePlacar() {
+  var corpoTabela = $(".placar").find("tbody");
+  var usuario = "Johan";
+  var numeroPalavras = $("#contador-palavras").text();  
+  var linha = novaLinha(usuario, numeroPalavras);
+  linha.find(".botao-remover").click(removeLinha)
 
+  corpoTabela.prepend(linha)
+};
+
+function novaLinha(usuario,palavras) {
+  var linha = $("<tr>");
+  var colunaUsuario = $("<td>").text(usuario);
+  var colunaPalavras = $("<td>").text(palavras);
+  var colunaRemover = $("<td>");
+  
+  var link = $("<a>").addClass("botao-remover").attr("href", "#");
+  var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+  link.append(icone);
+  colunaRemover.append(link);
+
+  linha.append(colunaUsuario);
+  linha.append(colunaPalavras);
+  linha.append(colunaRemover);
+
+  return linha;
+
+};
+function removeLinha() {   
+    event.preventDefault();
+    $(this).parent().parent().remove()
+};
 
 function reniciaJogo() {
   $("#reiniciar").click(function(){
